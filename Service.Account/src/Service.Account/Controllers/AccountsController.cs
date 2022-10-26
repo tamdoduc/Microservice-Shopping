bb272df614e3672992f9b35047aff34dcd6b4a36
tamdoduc <82079157+tamdoduc@users.noreply.github.com>
2022-10-26 11:30:51 +0700

    Fix AccountsCtroller

--- Service.Account/src/Service.Account/Controllers/AccountsController.cs
+++ Service.Account/src/Service.Account/Controllers/AccountsController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Service.Account.Entities;
+using Service.Account;
 using Base.Service;
 using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using Service.Account.Dtos;
+using System.Linq;
 
 namespace Base.Service.Controllers
 {
@@ -18,18 +20,18 @@ namespace Base.Service.Controllers
         {
             this.accountsRepository = accountsRepository;
         }
-    }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AccountDto>>> GetAsync(Guid userId)
-    {
-        if (userId == Guid.Empty)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccountDto>>> GetAsync(Guid userId)
         {
-            return BadRequest();
-        }
+            if (userId == Guid.Empty)
+            {
+                return BadRequest();
+            }
 
-        var accounts = (
-            await accountsRepository.GetAllAsync(account => account.Id == userId)
-        ).Select(account => account.AsDto());
+            var accounts = (
+                await accountsRepository.GetAllAsync(account => account.Id == userId)
+            ).Select(account => account.AsDto());
+        }
     }
 }
