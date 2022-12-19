ca70be04a21c29a56ce3eaf27a6868f18bef9716
tamdoduc <82079157+tamdoduc@users.noreply.github.com>
2022-12-19 11:21:55 +0700

    .

--- Exist-Project/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-function SearchType(type) 
-{
-    window.location = "../php/catalog.php?searchValue="+type;
-}
-function SearchBestSeller()
-{
-    window.location = "../php/catalog.php?typeSort=sortBestSeller";
-}
--- Exist-Project/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
-    $_SESSION['idAccount'] = -1;
-    unset($_SESSION['idAccount']);
-    header("Location:login.php");
-?>
